bee8121d7aa6b991a1a6d897f9d87d61001024ea
John Henry Abangon <147405154+johnhenryabangon@users.noreply.github.com>
2025-10-09 06:22:16 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+// Simple typing animation
+const text = ["Web Developer", "Cybersecurity Enthusiast", "Software Engineer"];
+let index = 0;
+let charIndex = 0;
+const typingText = document.querySelector(".typing-text");
+
+function type() {
+    if (charIndex < text[index].length) {
+        typingText.textContent += text[index].charAt(charIndex);
+        charIndex++;
+        setTimeout(type, 100);
+    } else {
+        setTimeout(erase, 2000);
+    }
+}
+
+function erase() {
+    if (charIndex > 0) {
+        typingText.textContent = text[index].substring(0, charIndex - 1);
+        charIndex--;
+        setTimeout(erase, 50);
+    } else {
+        index = (index + 1) % text.length;
+        setTimeout(type, 500);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    if (typingText) type();
+});
